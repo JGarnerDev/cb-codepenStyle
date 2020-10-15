5d272c4216fda1073d0731bacccc677daef44975
Jeffe <j.garner200@gmail.com>
2020-10-14 20:06:32 -0400

    Code / User class has methods for logging in, sending messages, logging out. They update the db properly

--- app.js
+++ app.js
@@ -23,14 +23,17 @@ class User {
     this.login();
   }
   login() {
+    //   Add the user to users db collection, keep the id from database
     this.id = usersRef.push(this).getKey();
+    //   Add the user to the current room
+    roomCurrentUsersRef(this.currentRoom).push(this);
   }
   logout() {
     usersRef.child(this.id).remove();
   }
   sendMessage(body) {
     const message = { name: this.name, body };
-    messageRef(this.currentRoom).push(message);
+    messageCurrentRoomRef(this.currentRoom).push(message);
   }
 }
 
@@ -39,9 +42,13 @@ let user;
 // Database references
 
 //      We will support multiple rooms, so the reference will be made dynamically
-const messageRef = (room) => {
+const messageCurrentRoomRef = (room) => {
   return db.ref(`/rooms/${room}/messages`);
 };
+const roomCurrentUsersRef = (room) => {
+  return db.ref(`/rooms/${room}/users`);
+};
+
 //      There will be another collection just for the users
 const usersRef = db.ref("/users");
 
@@ -55,18 +62,36 @@ const getEleByClass = (className) => {
   return document.getElementsByClassName(className);
 };
 
+// DOM Elements
 const loginPage = getEleById("login");
 const loginForm = getEleById("login-form");
 const loginButton = getEleById("login-submit");
 
 const chatPage = getEleById("chat");
+const chatHeader = getEleById("chat-header");
+const chatInput = getEleById("chat-input");
+const chatButton = getEleById("chat-submit");
+
+//      ...their event listeners
 
 loginButton.addEventListener("click", (e) => {
   e.preventDefault();
   // Take the username value from the form
   const username = loginForm.username.value;
-
+  // redefine the user variable as class object with the name
   user = new User(username);
+  // Take away the login page element from the DOM
   loginPage.remove();
+  // Bring the chat page element out of the hide class
   chatPage.classList.remove("hide");
+  // Change the chat header to the room the user is in
+  chatHeader.innerText = user.currentRoom;
+});
+
+chatButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  //   Take the message value from the form
+  const message = chatInput.value;
+  //   Use the class method to send the message
+  user.sendMessage(message);
 });
